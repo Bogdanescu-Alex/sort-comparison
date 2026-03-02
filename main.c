ce3c4db27cbb679d271c80193ed63b57a420ce30
Alex <wildshot71@gmail.com>
2026-03-02 18:55:58 +0200

    finished base

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include<stdlib.h>
 #include<stdio.h>
-#include<time.h>
+#if defined(_MSC_VER)
+# include <intrin.h>
+#else
+# include <x86intrin.h>
+#endif
 
-void sortFunc() {
+void insertSort(short *arr, short len) {
+    for (short i = 1; i < len; ++i) {
+        short key = *(arr + i);
+        short j = i - 1;
 
+        while (j >= 0 && *(arr + j) > key) {
+            *(arr + j + 1) = *(arr + j);
+            j = j - 1;
+        }
+        *(arr + j + 1) = key;
+    }
 }
 
 void generateArray(short *arr, short len) {
@@ -25,7 +38,17 @@ int main() {
     short n = 10000;
     short *arrayStart = (short *) malloc(n * sizeof(short));
     generateArray(arrayStart, n);
-    printArray(arrayStart, n);
+    
+    unsigned int dummy;
+    unsigned long long start = __rdtscp(&dummy);
+
+    insertSort(arrayStart, n);
+    
+    unsigned long long end = __rdtscp(&dummy);
+    long sortTime = end - start;
+
+    printf("TOOK %li CPU CYCLES", sortTime);
+    // printArray(arrayStart, n);
     free(arrayStart);
     return 0;
 }
